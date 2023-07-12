03280eafb9bc6d8ed8a5290f6a117475985b9c46
Dominic <dominicretli@slalom.com>
2023-07-12 19:59:04 -0400

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {PlanetsModule} from './planets-module/planets.module';
-import {DecimalSpacePipe} from './planets-module/pipes/decimal-space.pipe';
 
 @NgModule({
   declarations: [
